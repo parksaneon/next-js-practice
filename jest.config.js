f6c3a3b8cd7cc3be6585ee7e644af9cad210764e
parksaneon <sangeon1500@daum.net>
2021-09-30 10:10:16 +0900

    edit eslintrc.json

--- jest.config.js
+++ jest.config.js
@@ -22,4 +22,5 @@ module.exports = {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
   transformIgnorePatterns: ['/node_modules/', '^.+\\.module\\.(css|sass|scss)$'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
 };
